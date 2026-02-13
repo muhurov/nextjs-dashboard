725c9b869e6b903507bf7f02d9e58e24f0b4116f
muhurov <muhurov@gmail.com>
2026-02-13 15:50:27 +0300

    feat: Dashboard page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+import clsx from "clsx";
+
+import { inter } from "@/app/ui/fonts";
+import "@/app/ui/global.css";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(inter.className, "antialiased")}>{children}</body>
     </html>
   );
 }
